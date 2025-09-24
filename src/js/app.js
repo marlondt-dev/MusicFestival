ff8648bb47a1e2264a12b7d9571de3a4f5c1ee63
Marlon Torres <marlon.dt@hotmail.com>
2025-09-24 02:15:37 +0200

    feat(src): improved web performance with lazy loading and other image formats

--- src/js/app.js
+++ src/js/app.js
@@ -23,9 +23,12 @@ function createGaleria(){
     const galeria = document.querySelector('.galeria-imagenes')
 
     for (let i = 1; i <= CANTIDAD_IMAGENES; i++){
-    const imagen = document.createElement('IMG')
-    imagen.src = `src/img/gallery/full/${i}.jpg`
-    imagen.alt = `Imagen galeria ${i}`
+    const imagen = document.createElement('PICTURE')
+    imagen.innerHTML = `
+    <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+`;
     galeria.appendChild(imagen)
 
     imagen.onclick = function(){
@@ -35,9 +38,12 @@ function createGaleria(){
 }    
 
 function showImage(i){
-    const imagen = document.createElement('IMG')
-    imagen.src = `src/img/gallery/full/${i}.jpg`
-    imagen.alt = `Imagen galeria ${i}`
+    const imagen = document.createElement('PICTURE')
+    imagen.innerHTML = `
+    <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">
+`;
     
     //hacer el modal
 const modal = document.createElement('DIV')
